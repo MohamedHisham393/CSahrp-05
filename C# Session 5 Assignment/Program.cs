894fd282f4ab691ad066f7a16a8f7bcfd268f59d
MohamedHisham393 <mohamed.hisham12393@gmail.com>
2026-09-14 04:05:33 +0300

    Assinment 4 file - Q1

--- C# Session 5 Assignment/Program.cs
+++ C# Session 5 Assignment/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Assinment 4 file - Q1
+
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine(prices[1]);
         }
     }
 }
